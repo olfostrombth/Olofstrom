1f5fc20f3bc0d2db23fe0e61e9855f6ee38b1ce4
M3rli <M3rli.olsson@gmail.com>
2015-02-04 10:14:16 +0000

    upstreamupdate

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150203083607) do
-=======
 ActiveRecord::Schema.define(version: 20150203133552) do
->>>>>>> upstream/master
 
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
