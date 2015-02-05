09339905699f11fb3b50e93024e87ca46cbf7f50
kevin <kevinferm@gmail.com>
2015-02-05 11:21:28 +0100

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    gitkit_client = GitkitLib::GitkitClient.create_from_config_file '/home/kevin/Projects/Olofstrom/app/assets/gitkit-server-config.json'
+    #gitkit_client = GitkitLib::GitkitClient.create_from_config_file '/home/kevin/Projects/Olofstrom/app/assets/gitkit-server-config.json'
     token = request.cookies["gtoken"]
     @postBody = CGI.escape(request.body.read)
     gitkit_user,_ = gitkit_client.verify_gitkit_token token
