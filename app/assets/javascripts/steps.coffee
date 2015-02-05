68fe9e3909931f2238e2922d2579e3a83f7cc660
vagrant <vagrant@precise32.(none)>
2015-02-05 13:42:14 +0000

    fixat ny quizzesmigration

--- app/assets/javascripts/steps.coffee
+++ app/assets/javascripts/steps.coffee
@@ -2,3 +2,11 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+$ ->
+  $('#submit_quiz').click ->
+
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-<<<<<<< HEAD
-=======
-    #gitkit_client = GitkitLib::GitkitClient.create_from_config_file '/home/kevin/Projects/Olofstrom/app/assets/gitkit-server-config.json'
->>>>>>> upstream/master
+
     token = request.cookies["gtoken"]
     @postBody = CGI.escape(request.body.read)
     redirect_to root_path
