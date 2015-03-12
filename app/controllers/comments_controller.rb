a49afc6ccf4ebc8a8750632a7d79715a26a74565
mikaellisselgard <mikael.lisselgard@hotmail.se>
2015-03-12 19:40:17 +0000

    inplace edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,13 +43,16 @@ class CommentsController < ApplicationController
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
+    @comment = Comment.find(params[:id])
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        #format.json { render :show, status: :ok, location: @comment }
+        format.json { respond_with_bip(@comment) }
       else
         format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        #format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.json { respond_with_bip(@comment) }
       end
     end
   end
@@ -72,6 +75,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:msg, :category_id, :user_id, :parent_id, :row_order_position)
+      params.require(:comment).permit(:msg, :category_id, :user_id, :parent_id)
     end
 end
