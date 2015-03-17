7c84f4242ce5d11b09d3ce880341c04dd68352a6
mikaellisselgard <mikael.lisselgard@hotmail.se>
2015-03-17 08:46:16 +0000

    merge med kevin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '1050237569948-u2lbjttpbafobjd34bu9859aodhrftkc.apps.googleusercontent.com', 'yNKmshIuxNZGjQxuQ5k4q14A',
    {:provider_ignores_state => true,
      :image_aspect_ratio => "original",
-     :image_size => 250
+     :image_size => 150
   }
 end
